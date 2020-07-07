ce28570842d9b3f11fd07362f0b83f21e00c30d6
mostafamohamedmostafa <54216066+mostafamohamedmostafa@users.noreply.github.com>
2020-07-07 05:37:26 +0200

    finalize part 1

---
+++ app.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
+
+window.alert("Welcome to AWSOME FIX COMP");
+
+var username = prompt("What is YOur Name");
+
+alert("Hello " + username + "! How are you today?");
